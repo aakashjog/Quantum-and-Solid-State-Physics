e40718aacef76556e1663308a0ff696758b22667
aakashjog <aakashjog96@gmail.com>
2016-01-05 15:19:19 +0200

    SS Lecture 12 added

---
+++ Notes/Plots/expected_profile_with_constant_illumination_through_slit.m
@@ -0,0 +1,33 @@
+x = linspace(-5,5,10000);
+sigma_n = 0.4;
+sigma_p = 0.3335;
+shift = 0.2;
+
+fig = figure;
+hold on;
+plot(x,normpdf(x,0,sigma_n))
+plot(x,normpdf(x,0,sigma_p) - shift,'--')
+
+ax = gca;
+ax.TickLabelInterpreter = 'latex';
+ax.XTick = [];
+ax.XTickLabel = {};
+ax.YTick = [];
+ax.YTickLabel = {};
+
+xlabel('$x$','Interpreter','latex','FontSize',14);
+
+leg = legend('$\hat{n}(x)$','$\hat{p}(x)$');
+leg.Interpreter = 'latex';
+
+set(fig,'Units','Inches');
+pos = get(fig,'Position');
+set(fig,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)]);
+
+%matlab2tikz('graphical_solution_for_allowed_energy_levels_for_even_wave_functions_in_finite_potential_well.tikz')
+
+print(fig,'expected_profile_with_constant_illumination_through_slit','-dpdf','-r0')
+
+close all; clear; clc;
+
+! cp expected_profile_with_constant_illumination_through_slit.pdf ../fig-expected_profile_with_constant_illumination_through_slit.pdf
